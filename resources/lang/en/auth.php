cf27faa853673a594b43fbdb00e2b767acc2d250
Hafijul Islam <hafijul233@gmail.com>
2021-11-06 12:12:21 +0600

    user registration successful.

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,5 +16,8 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'register' => [
+        'success' => 'Registration successful. Welcome a aboard.',
+        'failed' => 'Registration Failed. Please try again later.'
+    ],
 ];
