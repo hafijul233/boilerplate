a3c3336aab81184cf8927b55f06c0052a63e65f9
Hafijul Islam <hafijul233@gmail.com>
2021-11-16 15:19:43 +0600

    config moved to env

--- config/media-library.php
+++ config/media-library.php
@@ -44,7 +44,7 @@ return [
     /*
      * By default all conversions will be performed on a queue.
      */
-    'queue_conversions_by_default' => env('QUEUE_CONVERSIONS_BY_DEFAULT', true),
+    'queue_conversions_by_default' => (bool)env('QUEUE_CONVERSIONS_BY_DEFAULT', true),
 
     /*
      * The fully qualified class name of the media model.
