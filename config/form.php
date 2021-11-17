9b4c588abcf0cd7a6ffb9c01b59c40f340572c21
Hafijul Islam <hafijul233@gmail.com>
2021-11-17 12:53:36 +0600

    form module config exported

---
+++ config/form.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Form',
+    /**
+     * Form Style Validation and Other Component will
+     * be selected form this section
+     * @var string style
+     * @value bootstrap3, bootstrap4, bootstrap5
+     */
+    'style' =>'bootstrap4'
+];
