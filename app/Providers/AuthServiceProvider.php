2d93e5afe448135da57f42413a13162752d6644e
Hafijul Islam <hafijul233@gmail.com>
2021-11-21 22:03:20 +0600

    Display Deleted Models in list

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Modules\Admin\Supports\DefaultValue;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,9 +27,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         // Implicitly grant "Super Admin" role all permissions
-        // This works in the app by using gate-related functions like auth()->user->can() and @can()
         Gate::before(function ($user, $ability) {
-            return $user->hasRole('Super Administration') ? true : null;
+            return $user->hasRole(DefaultValue::SUPER_ADMIN_ROLE) ? true : null;
         });
     }
 }
