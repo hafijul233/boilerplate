347d0712e0bb70ae47b4863c29766eae0271473d
Hafijul Islam <hafijul233@gmail.com>
2021-12-03 16:22:20 +0600

    converting core sending add module pointer to core

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(BladeServiceProvider::class);
-        $this->app->register(ModuleServiceProvider::class);
+
     }
 
     /**
