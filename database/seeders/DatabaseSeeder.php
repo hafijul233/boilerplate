23a8425fd182c0592065c400da2bb047d18eb477
Hafijul Islam <hafijul233@gmail.com>
2021-12-03 20:56:18 +0600

    custom html added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Modules\Admin\Database\Seeders\AdminDatabaseSeeder;
 use Modules\Auth\Database\Seeders\AuthDatabaseSeeder;
-use Modules\Rbac\Database\Seeders\RbacDatabaseSeeder;
+use Modules\Core\Database\Seeders\CoreDatabaseSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RbacDatabaseSeeder::class);
+        $this->call(CoreDatabaseSeeder::class);
         $this->call(AuthDatabaseSeeder::class);
 
     }
