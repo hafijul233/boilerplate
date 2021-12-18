947570af8233b4db6f46a186c3cd1ccbad26a639
Hafijul Islam <hafijul233@gmail.com>
2021-12-18 19:52:35 +0600

    resource added fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-/*        $this->call(CoreDatabaseSeeder::class);
+        $this->call(CoreDatabaseSeeder::class);
         $this->call(AuthDatabaseSeeder::class);
-        $this->call(ContactDatabaseSeeder::class);*/
-        $this->call(CitySeeder::class);
+        $this->call(ContactDatabaseSeeder::class);
+        //$this->call(CitySeeder::class);
     }
 }
